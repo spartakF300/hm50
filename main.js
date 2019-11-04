b480ba161ea295238d04eb5ad644a83ab4e9dcf6
Spartak Fomenko <spartakfomenko28@gmail.com>
2019-11-04 19:21:35 +0600

    add .ignore

---
+++ main.js
@@ -0,0 +1,100 @@
+class Product {
+    constructor(nameProduct, calories){
+
+        this.nameProduct = nameProduct;
+        this.calories = calories;
+    }
+}
+
+class Dish {
+    constructor(dishName) {
+        this.dishArray = [];
+        this.dishName = dishName;
+    }
+    addProduct(name , grams) {
+        this.dishArray.push({name, grams});
+        // console.log(this.dishArray);
+        // console.log(name);
+    }
+    getCalories(){
+        this.allCalories = [];
+        let sumCalories = 0;
+        for(let i = 0; i < this.dishArray.length; i++){
+            let sumDish = (this.dishArray[i].name.calories * this.dishArray[i].grams) / 100;
+            sumCalories+= sumDish
+        }
+        this.allCalories.push(sumCalories);
+        return  this.allCalories
+    }
+
+}
+class CaloriesCalculator {
+    constructor(){
+        this.newDish =[]
+    }
+    addDish(dish) {
+        this.newDish.push(dish);
+        // console.log(this.newDish);
+    }
+    getTotalCalories(){
+        let calories = 0;
+        this.newDish.forEach(dish => {
+            calories += dish.getCalories();
+        })
+        return calories;
+    }
+    getAllDishesInfo() {
+        console.log(this.newDish[0]);
+        let a = []
+        for(let i = 0; i < this.newDish[0].dishArray.length; i++){
+            let aa = this.newDish[0].dishArray[i].grams;
+            let ss = this.newDish[0].dishArray[i].name.nameProduct;
+            let gg = this.newDish[0].dishArray[i].name.calories
+            a.push(ss + ', ' + aa + 'гр ' + gg + 'ккал\n');
+
+
+
+        }
+        console.log('============================================');
+        const dishes = this.newDish[0].dishName + ' 1'  + ' порция, ' + calories + ' ккал' + '\n' + ',' + a;
+        console.log(dishes);
+        console.log('============================================');
+    }
+
+}
+
+const meat = new Product('Филе говядина', 158);
+
+const rice = new Product('Рис', 130);
+
+const onion = new Product('Лук', 40);
+
+const carrot = new Product('Морковь', 41);
+
+
+
+const plov = new Dish('Плов');
+
+plov.addProduct(meat, 100);
+//
+plov.addProduct(rice, 150);
+
+plov.addProduct(onion, 25);
+
+plov.addProduct(carrot, 25);
+plov.getCalories()
+
+
+const calculator = new CaloriesCalculator();
+
+calculator.addDish(plov);
+
+const calories = calculator.getTotalCalories();
+
+console.log(calories); // должно вывести 373.25
+
+
+
+const totals = calculator.getAllDishesInfo();
+
+console.log(totals);
